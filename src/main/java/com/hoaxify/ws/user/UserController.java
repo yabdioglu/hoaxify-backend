942b7e11cf70172c7e69d83d61bbb1d9ad2a436d
yabdioglu <yabdioglu37@gmail.com>
2022-02-21 13:11:47 +0300

    0208-refactor-user-controller

--- src/main/java/com/hoaxify/ws/user/UserController.java
+++ src/main/java/com/hoaxify/ws/user/UserController.java
@@ -1,21 +1,21 @@
 package com.hoaxify.ws.user;
 
+import com.hoaxify.ws.shared.GenericResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class UserController {
 
     @Autowired
-    UserRepository userRepository;
+    UserService userService;
 
     @PostMapping("/api/1.0/users")
-    public void createUser(@RequestBody User user){
-        userRepository.save(user);
+    public GenericResponse createUser(@RequestBody User user){
+        userService.save(user);
+        return new GenericResponse("user created");
     }
 }
