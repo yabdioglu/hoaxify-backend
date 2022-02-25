9fd1155d744263bed445666f61542b05e8f6a71b
yabdioglu <yabdioglu37@gmail.com>
2022-02-25 21:29:21 +0300

    0309-unique-username-constraint

---
+++ src/main/java/com/hoaxify/ws/user/UniqueUsername.java
@@ -0,0 +1,24 @@
+package com.hoaxify.ws.user;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import javax.validation.constraints.NotNull;
+import java.lang.annotation.Documented;
+import java.lang.annotation.Repeatable;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.*;
+import static java.lang.annotation.ElementType.TYPE_USE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+@Target({ FIELD })
+@Retention(RUNTIME)
+@Constraint(validatedBy = { UniqueUsernameValidator.class })
+public @interface UniqueUsername {
+    String message() default "Username must be unique";
+
+    Class<?>[] groups() default { };
+
+    Class<? extends Payload>[] payload() default { };
+}
