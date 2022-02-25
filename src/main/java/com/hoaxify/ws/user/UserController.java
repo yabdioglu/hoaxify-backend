e3e40d1e8b1fc1d7b207c3c61e040c4e3d053c5e
yabdioglu <yabdioglu37@gmail.com>
2022-02-25 17:16:39 +0300

    0303-validate-display-name

--- src/main/java/com/hoaxify/ws/user/UserController.java
+++ src/main/java/com/hoaxify/ws/user/UserController.java
@@ -21,11 +21,21 @@ public class UserController {
 
     @PostMapping("/api/1.0/users")
     public ResponseEntity<?> createUser(@RequestBody User user){
+        ApiError error = new ApiError(400,"Validation error", "/api/1.0/users");
+        Map<String, String> validationErrors = new HashMap<>();
+
         String username = user.getUsername();
+        String displayName = user.getDisplayName();
+
         if(username == null || username.isEmpty()){
-            ApiError error = new ApiError(400,"Validation error", "/api/1.0/users");
-            Map<String, String> validationErrors = new HashMap<>();
             validationErrors.put("username", "username cannot be null");
+        }
+
+        if(displayName == null || displayName.isEmpty()){
+            validationErrors.put("displayName", "cannot be null");
+        }
+
+        if(validationErrors.size() > 0){
             error.setValidationErrors(validationErrors);
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
         }
