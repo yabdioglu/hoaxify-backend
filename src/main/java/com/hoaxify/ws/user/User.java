c22bf100006f5d15b67a896a5c55ec51e9668b2b
yabdioglu <yabdioglu37@gmail.com>
2022-02-25 18:45:37 +0300

    0307-remaining-validations

--- src/main/java/com/hoaxify/ws/user/User.java
+++ src/main/java/com/hoaxify/ws/user/User.java
@@ -7,6 +7,8 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
 @Data
 @Entity
@@ -17,11 +19,16 @@ public class User {
     private long id;
 
     @NotNull
+    @Size(min = 4, max = 255)
     private String username;
 
     @NotNull
+    @Size(min = 4, max = 255)
     private String displayName;
 
+    @NotNull
+    @Size(min = 8, max = 255)
+    @Pattern(regexp="^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).*$")
     private String password;
 
 }
