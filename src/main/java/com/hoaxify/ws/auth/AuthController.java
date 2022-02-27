8b4d2c78a55ec91628a5796bf37e1daacdcc3428
yabdioglu <yabdioglu37@gmail.com>
2022-02-27 14:28:09 +0300

    0508-login-error-to-api-error

--- src/main/java/com/hoaxify/ws/auth/AuthController.java
+++ src/main/java/com/hoaxify/ws/auth/AuthController.java
@@ -11,12 +11,10 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Base64;
 import java.util.HashMap;
@@ -28,31 +26,14 @@ public class AuthController {
     @Autowired
     UserRepository userRepository;
 
-    PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-
     @PostMapping("/api/1.0/auth")
     @JsonView(Views.Base.class)
-    ResponseEntity<?> handleAuthentication(@RequestHeader(name = "Authorization", required = false) String authorization) {
-        if (authorization == null) {
-            ApiError error = new ApiError(401, "Unauthorized request", "/api/1.0/auth");
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(error);
-        }
+    ResponseEntity<?> handleAuthentication(@RequestHeader(name = "Authorization") String authorization) {
         String base64encoded = authorization.split("Basic ")[1];// dXNlcjE6UDRzc3dvcmQ=
         String decoded = new String(Base64.getDecoder().decode(base64encoded)); // user1:P4ssword
         String[] parts = decoded.split(":"); // ["user1", "P4ssword"]
         String username = parts[0];
-        String password = parts[1];
         User inDB = userRepository.findByUsername(username);
-        if (inDB == null) {
-            ApiError error = new ApiError(401, "Unauthorized request", "/api/1.0/auth");
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(error);
-        }
-        String hashedPassword = inDB.getPassword();
-        if(!passwordEncoder.matches(password, inDB.getPassword())){
-            ApiError error = new ApiError(401, "Unauthorized request", "/api/1.0/auth");
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(error);
-        }
-
         return ResponseEntity.ok(inDB);
     }
 
