755eb4762534ba0e419d5fa011b4b12711be9ece
yabdioglu <yabdioglu37@gmail.com>
2022-03-12 11:34:05 +0300

    1205-updating-another-user

--- src/main/java/com/hoaxify/ws/configuration/SecurityConfiguration.java
+++ src/main/java/com/hoaxify/ws/configuration/SecurityConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -21,6 +22,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Autowired
     UserAuthService userAuthService;
@@ -36,7 +38,9 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
         //Spring'e Authentication kontrolü yap diyoruz.
         http
-                .authorizeRequests().antMatchers(HttpMethod.POST, "/api/1.0/auth").authenticated()
+                .authorizeRequests()
+                    .antMatchers(HttpMethod.POST, "/api/1.0/auth").authenticated()
+                    .antMatchers(HttpMethod.PUT, "/api/1.0/users/{username}").authenticated()
                 .and()
                 .authorizeRequests().anyRequest().permitAll();
 
@@ -49,8 +53,9 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         auth.userDetailsService(userAuthService).passwordEncoder(passwordEncoder());
     }
 
-    @Bean // BCryptPasswordEncoder'ı birden fazla yerde kullanmamıza yarar
-    PasswordEncoder passwordEncoder(){
+    @Bean
+        // BCryptPasswordEncoder'ı birden fazla yerde kullanmamıza yarar
+    PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 }
--- src/main/java/com/hoaxify/ws/user/UserController.java
+++ src/main/java/com/hoaxify/ws/user/UserController.java
@@ -1,5 +1,6 @@
 package com.hoaxify.ws.user;
 
+import com.hoaxify.ws.error.ApiError;
 import com.hoaxify.ws.shared.CurrentUser;
 import com.hoaxify.ws.shared.GenericResponse;
 import com.hoaxify.ws.user.vm.UserUpdateVM;
@@ -8,6 +9,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -20,23 +24,25 @@ public class UserController {
     UserService userService;
 
     @PostMapping("/users")
-    public GenericResponse createUser(@Valid @RequestBody User user){
+    public GenericResponse createUser(@Valid @RequestBody User user) {
         userService.save(user);
         return new GenericResponse("user created");
     }
 
     @GetMapping("/users")
-    Page<UserVM> getUsers(Pageable pageable, @CurrentUser User user){
+    Page<UserVM> getUsers(Pageable pageable, @CurrentUser User user) {
         return userService.getUsers(pageable, user).map(UserVM::new);
     }
 
-    @GetMapping("/users/{username}") //urldeki username'i parametre olarak alırız.(PathVariable)
-    UserVM getUser(@PathVariable String username){
+    @GetMapping("/users/{username}")
+        //urldeki username'i parametre olarak alırız.(PathVariable)
+    UserVM getUser(@PathVariable String username) {
         User user = userService.getByUsername(username);
         return new UserVM(user);
     }
 
     @PutMapping("/users/{username}")
+    @PreAuthorize("#username == principal.username") // principal loggedInUser olarak düşünebiliriz.
     UserVM updateUser(@PathVariable String username, @RequestBody UserUpdateVM updatedUser) {
         User user = userService.updateUser(username, updatedUser);
         return new UserVM(user);
