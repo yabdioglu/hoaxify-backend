a65c7289a271d6d5a3a2f8c9c4ed03b04f923df0
yabdioglu <yabdioglu37@gmail.com>
2022-03-21 17:46:50 +0300

    1505-validation

--- src/main/java/com/hoaxify/ws/hoax/HoaxController.java
+++ src/main/java/com/hoaxify/ws/hoax/HoaxController.java
@@ -2,10 +2,11 @@ package com.hoaxify.ws.hoax;
 
 import com.hoaxify.ws.shared.GenericResponse;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/1.0")
@@ -15,7 +16,7 @@ public class HoaxController {
     HoaxService hoaxService;
 
     @PostMapping("/hoaxes")
-    public GenericResponse saveHoax(@RequestBody Hoax hoax){
+    public GenericResponse saveHoax(@Valid @RequestBody Hoax hoax){
         hoaxService.save(hoax);
         return new GenericResponse("Hoax is saved");
     }
