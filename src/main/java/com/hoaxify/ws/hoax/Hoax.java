b91776c3759a0e43e654362482b4523935114316
yabdioglu <yabdioglu37@gmail.com>
2022-03-21 15:27:08 +0300

    1504-save-hoax-to-db

---
+++ src/main/java/com/hoaxify/ws/hoax/Hoax.java
@@ -0,0 +1,21 @@
+package com.hoaxify.ws.hoax;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Data
+@Entity
+public class Hoax {
+
+    @Id
+    @GeneratedValue
+    private long id;
+
+    private String content;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date timestamp;
+
+}
