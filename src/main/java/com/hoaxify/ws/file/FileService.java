ad690fe08ef12c03d93d893c70139594f74bc6e8
yabdioglu <yabdioglu37@gmail.com>
2022-03-27 13:34:28 +0300

    1906-scheduled-service

--- src/main/java/com/hoaxify/ws/file/FileService.java
+++ src/main/java/com/hoaxify/ws/file/FileService.java
@@ -2,6 +2,8 @@ package com.hoaxify.ws.file;
 
 import com.hoaxify.ws.configuration.AppConfiguration;
 import org.apache.tika.Tika;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -13,9 +15,11 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Base64;
 import java.util.Date;
+import java.util.List;
 import java.util.UUID;
 
 @Service
+@EnableScheduling
 public class FileService {
 
     AppConfiguration appConfiguration;
@@ -76,5 +80,15 @@ public class FileService {
         attachment.setName(fileName);
         attachment.setDate(new Date());
         return fileAttachmentRepository.save(attachment);
+    }
+
+    @Scheduled(fixedRate = 24 * 60 * 60 * 1000) // 24 saatte bir bu method koşacak (milisaniye cinsi)
+    public void cleanupStorage() {
+        Date twentyFourHoursAgo = new Date(System.currentTimeMillis() - (24 * 60 * 60 * 1000));
+        List<FileAttachment> filesToBeDeleted =  fileAttachmentRepository.findByDateBeforeAndHoaxIsNull(twentyFourHoursAgo);
+        for(FileAttachment file : filesToBeDeleted){
+            deleteFile(file.getName());  // delete file
+            fileAttachmentRepository.deleteById(file.getId()); // delete from table
+        }
     }
 }
