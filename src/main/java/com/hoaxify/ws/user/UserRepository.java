a0da0be459f29ba0e6b299a50a0552267f19b93d
yabdioglu <yabdioglu37@gmail.com>
2022-04-01 20:33:12 +0300

    2009-custom-delete-repo-method

--- src/main/java/com/hoaxify/ws/user/UserRepository.java
+++ src/main/java/com/hoaxify/ws/user/UserRepository.java
@@ -5,8 +5,13 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import javax.transaction.Transactional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByUsername(String username);
 
     Page<User> findByUsernameNot(String username, Pageable page); // Bu username'e sahip olmayanları getir.
+
+    @Transactional // Transaction yoksa Spring bizim yerimize bir transaction oluşturuyor.
+    void deleteByUsername(String username);
 }
--- src/main/java/com/hoaxify/ws/user/UserService.java
+++ src/main/java/com/hoaxify/ws/user/UserService.java
@@ -72,7 +72,6 @@ public class UserService {
 
     public void deleteUser(String username) {
         hoaxService.deleteHoaxesOfUser(username);
-        User inDB = userRepository.findByUsername(username);
-        userRepository.delete(inDB);
+        userRepository.deleteByUsername(username);
     }
 }
